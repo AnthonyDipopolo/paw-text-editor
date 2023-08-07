0c0ab36f3730002c7318a520565640722b9800cc
AnthonyDipopolo <anthony.dipopolo@gmail.com>
2023-08-07 16:19:03 -0400

    fixed webpack config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,28 +3,49 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // HtmlWebpackPlugin for generating HTML files
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        chunks: ['main'],
+      }),
+      // WebpackPwaManifest for generating the manifest file
+      new WebpackPwaManifest({
+        name: 'Text-Editor App',
+        short_name: 'Text App',
+        description: 'My Progressive Text-Editor App',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: path.resolve('favicon.ico'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
+      // InjectManifest for generating the service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
     ],
 
     module: {
-      rules: [
-        
-      ],
+      rules: [{
+        test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+    }],
     },
   };
 };
